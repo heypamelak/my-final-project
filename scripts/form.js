87fd8a4cbc9c4fb10c3688e1e244649755869245
Pamela Krikorian <heypamelak@gmail.com>
2017-08-22 20:54:56 -0500

    trying to start form and work image hover effects

---
+++ scripts/form.js
@@ -0,0 +1,8 @@
+//the form
+window.onload = function(event) {
+
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var messageInput = document.getElementById('message');
+
+}

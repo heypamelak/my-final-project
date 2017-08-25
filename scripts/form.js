2e345805e1af7c73e39447da9ecbb4026566d8a7
Pamela Krikorian <heypamelak@gmail.com>
2017-08-24 20:47:37 -0500

    fixed some css breakage; james helped with jquery form

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,24 @@
 //the form
-window.onload = function(event) {
 
-  var nameInput = document.getElementById('name');
-  var emailInput = document.getElementById('email');
-  var messageInput = document.getElementById('message');
+$(document).ready(function(event) {
 
-}
+  // $("#contact-form").submit(function(event){
+  //   event.preventDefault();
+  //   $.ajax({
+  //     url: "https://www.enformed.io/uaic04k1",
+  //     method: "post",
+  //     dataType: "jsonp",
+  //     accepts: "application/json",
+  //     data: $("#contact-form").serialize(),
+  //     success: function(data){
+  //       console.log("Your form was successfully received!");
+  //       window.location.href = 'http://google.com';
+  //     },
+  //     error: function(err){
+  //       console.log("Failure. Try again.", err);
+  //       //window.location.href = 'http://google.com/error';
+  //     }
+  //   });
+  // });
+
+});
